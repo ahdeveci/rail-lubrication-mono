2550f118d0c9a723ceb38d74febb19d70be90de3
Ahmet Deveci <ahmet.deveci@commencis.com>
2025-03-11 23:27:26 +0300

    chore: add koa server initial settings

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,13 +1,26 @@
+import 'reflect-metadata';
 import koa from 'koa';
+import {
+  useContainer,
+  useKoaServer,
+  RoutingControllersOptions,
+} from 'routing-controllers';
+import { Container } from 'typedi';
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
+const routingOptions: RoutingControllersOptions = {
+  routePrefix: '/api',
+  controllers: [`${__dirname}/controllers/**/*.controller.[tj]s`],
+  middlewares: [`${__dirname}/middlewares/**.[tj]s`],
+  defaultErrorHandler: false,
+};
+
 const app = new koa();
 
-app.use(async (ctx) => {
-  ctx.body = { message: 'Hello API' };
-});
+useContainer(Container);
+useKoaServer(app, routingOptions);
 
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
