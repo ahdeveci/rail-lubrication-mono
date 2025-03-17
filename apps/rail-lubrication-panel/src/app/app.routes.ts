0f18956c1c582e51e263e3f456ccfc5c1aca1375
Ahmet Deveci <ahmet.deveci@commencis.com>
2025-03-18 00:47:16 +0300

    feat: add routes

--- apps/rail-lubrication-panel/src/app/app.routes.ts
+++ apps/rail-lubrication-panel/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Route } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () =>
+          import('./pages/dashboard/routes').then((m) => m.routes),
+      },
+    ],
+  },
+  { path: '**', redirectTo: 'dashboard' },
+];
