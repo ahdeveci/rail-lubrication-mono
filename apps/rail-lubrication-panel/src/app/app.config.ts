a242b95ac00fa4bc4fc484a465ef7bccc1d4095b
Ahmet Deveci <ahmet.deveci@commencis.com>
2025-03-18 00:45:17 +0300

    refactor: refactor app component and delete redundant files

--- apps/rail-lubrication-panel/src/app/app.config.ts
+++ apps/rail-lubrication-panel/src/app/app.config.ts
@@ -1,12 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+  withHashLocation,
+  withInMemoryScrolling,
+  withRouterConfig,
+  withViewTransitions,
+} from '@angular/router';
 import { appRoutes } from './app.routes';
-import {provideHttpClient} from "@angular/common/http";
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(
+      appRoutes,
+      withRouterConfig({
+        onSameUrlNavigation: 'reload',
+      }),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      }),
+      withEnabledBlockingInitialNavigation(),
+      withViewTransitions(),
+      withHashLocation()
+    ),
     provideHttpClient(),
   ],
 };
