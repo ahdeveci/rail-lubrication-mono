7413d56f9e450ba3af37d075b09ffb23f55a2d09
Ahmet Deveci <ahmet.deveci@commencis.com>
2025-03-21 00:20:33 +0300

    refactor: refactor main layout component

--- apps/rail-lubrication-panel/src/app/layouts/main-layout/main-layout.component.ts
+++ apps/rail-lubrication-panel/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
-import { ContainerComponent } from '@coreui/angular';
-import { RouterOutlet } from '@angular/router';
+import {
+  ContainerComponent,
+  ShadowOnScrollDirective,
+  SidebarBrandComponent,
+  SidebarComponent,
+  SidebarFooterComponent,
+  SidebarHeaderComponent,
+  SidebarNavComponent,
+  SidebarNavHelper,
+  SidebarToggleDirective,
+  SidebarTogglerDirective,
+} from '@coreui/angular';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { NgScrollbar } from 'ngx-scrollbar';
+import { navItems } from './_nav';
+import { IconDirective } from '@coreui/icons-angular';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [HeaderComponent, FooterComponent, ContainerComponent, RouterOutlet],
+  imports: [
+    SidebarComponent,
+    SidebarHeaderComponent,
+    SidebarBrandComponent,
+    SidebarNavComponent,
+    SidebarFooterComponent,
+    SidebarToggleDirective,
+    SidebarTogglerDirective,
+    ContainerComponent,
+    IconDirective,
+    NgScrollbar,
+    RouterOutlet,
+    RouterLink,
+    ShadowOnScrollDirective,
+    HeaderComponent,
+    FooterComponent,
+  ],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.css',
+  styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  public navItems = [...navItems];
+}
